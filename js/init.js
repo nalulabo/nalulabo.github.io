7dbe42f656a0aa7eac81d25ee00f1ea8c6a46135
unknown <>
2017-04-07 17:30:17 +0900

    initial commit

---
+++ js/init.js
@@ -0,0 +1,21 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    console.log("%c============================================================", "color: #11ccff;");
+    console.log("");
+    console.log("%c  #    #   ##   #      #    # #        ##   #####   ####", "color: #11ccff;");
+    console.log("%c  ##   #  #  #  #      #    # #       #  #  #    # #    #", "color: #11ccff;");
+    console.log("%c  # #  # #    # #      #    # #      #    # #####  #    #", "color: #11ccff;");
+    console.log("%c  #  # # ###### #      #    # #      ###### #    # #    #", "color: #11ccff;");
+    console.log("%c  #   ## #    # #      #    # #      #    # #    # #    #", "color: #11ccff;");
+    console.log("%c  #    # #    # ######  ####  ###### #    # #####   ####", "color: #11ccff;");
+    console.log("");
+    console.log("%c  ようこそ、なるーらぼへ", "color: #ffaa00;");
+    console.log("%c  自分のやりたいことを実現できるようにするために", "color: #ffaa00;");
+    console.log("%c  自分がやりたいようにそれを実行できるようにするために。", "color: #ffaa00;");
+    console.log("%c  一緒に学んでいきましょう。", "color: #ffaa00;");
+    console.log("%c============================================================", "color: #11ccff;");
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
